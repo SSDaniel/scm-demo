9cafbdad98e828dfff15ed5ad94aa8c8fa8c1f89
SSDaniel <danielsarmento9@gmail.com>
2023-08-15 21:51:45 -0300

    ADICIONADA A FUNÇÃO DE CALCULO DE RAIZ QUADRADA DE UM NUMERO

--- scm.c
+++ scm.c
@@ -10,11 +10,11 @@ void soma(int a, int b) {
 void data_hora(){
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    printf("Agora: %d-%02d-%02d %02d:%02d:%02d\n\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour -3, tm.tm_min, tm.tm_sec);
+    printf("Agora: %d-%02d-%02d %02d:%02d:%02d\n\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour - 3, tm.tm_min, tm.tm_sec);
 }
 
 void potencia(int a, int b) {
-    printf("%d^%d = %f\n", a,b, pow(a,b));
+    printf("%d^%d = %f\n", a, b, pow(a, b));
 }
 
 void divide(int a, int b) {
@@ -30,6 +30,13 @@ void subtrai(int a, int b) {
     printf("%d - %d = %d\n", a, b, a - b);
 }
 
+void raiz_quadrada(double num) {
+    if (num < 0) {
+        printf("Não é possível calcular a raiz quadrada de um número negativo.\n");
+    } else {
+        printf("Raiz quadrada de %.2f = %.2f\n", num, sqrt(num));
+    }
+}
 
 int main() {
     data_hora();
@@ -42,5 +49,12 @@ int main() {
     divide(6, 3);
     divide(6, 0);
     subtrai(5, 3);
+
+    double numero;
+    printf("Insira um número para calcular a raiz quadrada: ");
+    scanf("%lf", &numero);
+    raiz_quadrada(numero);
+
     return 0;
 }
+
